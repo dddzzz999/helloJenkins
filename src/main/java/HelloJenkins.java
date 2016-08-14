6ef618761d7e3d3b5c25ab61e30d5710dc36e736
dz <dddzzz999@yahoo.com>
2016-08-14 11:50:04 -0400

    c

--- src/main/java/HelloJenkins.java
+++ src/main/java/HelloJenkins.java
@@ -1,5 +1,4 @@
 
-
 public class HelloJenkins {
     public boolean someLibraryMethod() {
         return true;
@@ -7,5 +6,6 @@ public class HelloJenkins {
     
     public static void main(String[] args) {
 			System.out.println("Hello Jenkins!");
+			System.out.println("Hello Jenkins again!");
 		}
 }
