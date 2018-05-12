0659451c7cc4f9413993765c0411d11dc13bddbf
dz <dddzzz999@yahoo.com>
2018-05-12 09:41:43 -0400

    remove import in test files

--- src/test/java/HelloJenkinsTest.java
+++ src/test/java/HelloJenkinsTest.java
@@ -1,9 +1,8 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class HelloJenkinsTest {
-    @Test public void testSomeLibraryMethod() {
+    public void testSomeLibraryMethod() {
     	HelloJenkins classUnderTest = new HelloJenkins();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+    
+    
 }
